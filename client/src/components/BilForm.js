e8f76c517d5ddc56bd2014c64e31d749912c1eaf
EFinish <EFinish@gmail.com>
2020-09-15 21:03:05 +0300

    tidy up form and some docker work

--- client/src/components/BilForm.js
+++ client/src/components/BilForm.js
@@ -1,24 +1,68 @@
 import React from 'react';
-import BilRegNummer from './BilRegNummer';
-import DinBonus from './DinBonus';
-import Fødselsnummer from './Fødselsnummer';
-import Navn from './Navn';
-import Epost from './Epost';
 
 export default class BilForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            bilreg: null,
+            bonus: null,
+            fnummer: null,
+            fornavn: null,
+            etternavn: null,
+            epost: null
+        };
+    }
+
+    handleSubmit(event) {
+        console.log(this.state);
+    }
+
     render() {
         return (
             <form>
-                <BilRegNummer></BilRegNummer>
-                <DinBonus></DinBonus>
-                <Fødselsnummer></Fødselsnummer>
-                <Navn></Navn>
-                <Epost></Epost>
                 <div>
-                    <button type="submit">Beregn pris</button>
+                    <label>
+                        Bilens registreringsnummer
+                        <input required type="text" value={this.state.bilreg}></input>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Din bonus
+                        <select required value={this.state.bonus}>
+                            <option value="VDB">Velg din bonus</option>
+                        </select>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Fødselsnummer
+                        <input required type="date" value={this.state.fnummer}></input>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Fornavn
+                        <input required type="text" value={this.state.fornavn}></input>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Etternavn
+                        <input required type="text" value={this.state.etternavn}></input>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Epost
+                        <input required type="email" value={this.state.epost}></input>
+                    </label>
+                </div>
+                <div>
+                    <button type="button" onClick={this.handleSubmit}>Beregn pris</button>
                     <button type="button">Avbryt</button>
                 </div>
             </form>
-            );
+        );
     }
 }
